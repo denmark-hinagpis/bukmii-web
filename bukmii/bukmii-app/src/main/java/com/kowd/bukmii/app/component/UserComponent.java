685c15d6d29d866e85b1c358f5ea0529d2ed5397
dhinagpis <denmark.hinagpis@gmail.com>
2016-12-24 20:30:17 +0800

    commit and push

--- bukmii/bukmii-app/src/main/java/com/kowd/bukmii/app/component/UserComponent.java
+++ bukmii/bukmii-app/src/main/java/com/kowd/bukmii/app/component/UserComponent.java
@@ -26,7 +26,9 @@ public class UserComponent extends BaseComponent {
 		final String encryptedPass = Crypt.encryptHexString(password);
 		if (null != user && user.getPassword().equals(encryptedPass)) {
 			return convertBeanToFormBean(user);
-		} //change
+		} //ohayou jhg
+		
+		//My new changes
 
 		return null;
 	}
